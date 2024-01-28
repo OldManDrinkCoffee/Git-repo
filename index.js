9009922c8b0214e1572c7920c268a53cb496c817
Danh <nguyenhoangcongdanh2075@gmail.com>
2024-01-28 13:02:02 +0700

    Hi

---
+++ index.js
@@ -0,0 +1,50 @@
+// fetch = function used for making Http requests to fetch resources
+//         (JSON style data, images, files)
+//          Simplifies asynchronous data fetching in JS and 
+//          used for interacting with APIs to retrieve and send
+//          data aynchronously over the web.
+//          fetch(url, {options})
+
+
+
+// async function fetchData(){
+
+//     try{
+
+//         const pokemonName = document.getElementById("pokemonName").value.toLowerCase()
+//         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+
+//         if(!response.ok) {
+//             throw new Error("Could not fetch resource");
+//         }
+//         const data = await response.json();
+//         const pokemonSprite = data.sprites.front_default
+//         const imgElement = document.getElementById("pokemonSprite");
+//         imgElement.src = pokemonSprite;
+//         imgElement.style.display = "block";
+//     }
+//     catch(error){
+//         console.error(error);
+//     }
+// }
+
+
+async function fetchData() {
+    try{
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+
+        if(!response.ok) {
+            throw new Error("Could not fetch resource");
+        }
+        const data = await response.json();
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite")
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
+
+    }
+    catch(error){
+        console.error(error)
+    }
+}
